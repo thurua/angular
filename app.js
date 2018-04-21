91eabae1e44b3cbbf823b44959345fbdc306f148
thurua <thurua19091999@gmail.com>
2018-04-21 17:45:59 +0700

    Add configure to deploy to Heroku

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 4200;
+const server = http.createServer(app);
+
+app.use(express.static(path.join(__dirname, '/dist')));
+app.all('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/dist/index.html'));
+});
